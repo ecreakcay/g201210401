49595568956a156d5025f60e619d89783f9f65fd
Ecre Aynur AKÇAY <ecreakcay@gmail.com>
2021-05-15 23:04:41 +0300

    son hal

--- boostrapsite/iletisim/iletisim.php
+++ boostrapsite/iletisim/iletisim.php
@@ -1,3 +1,38 @@
+<?php
+
+if(isset($_POST["email"])){
+	
+	$kime= "ecreakcay@gmail.com";
+	$konu= $_POST["subject"];
+	$mesaj="<h1>".$_POST["message"]."<h1>";
+	
+	$baslik = "From: ".$_POST["name"]."<".$_POST["email"]."> \r\n";
+	
+	$baslik .="Reply-to : reply@ecreakcay.com \r\n"; //\r satır sonuna geldim, \n bi alt satıra geç demek
+	
+	$baslik .= "Content-type: text/html \r\n";
+	
+	if(mail($kime,$konu,$mesaj,$baslik)){
+		echo"Formunuz gönderilmiştir..";
+	}
+	else{
+		echo "Hata! Form gönderilemedi..";
+	}
+	
+	
+	
+	
+}
+
+
+
+?>
+
+
+
+
+
+
 <!doctype html>
 <html lang="en">
 
